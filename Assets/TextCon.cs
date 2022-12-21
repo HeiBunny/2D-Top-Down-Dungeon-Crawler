6a8c6f9cc927d9bec0e8136586a83b812820d696
Loneox01 <dlzhang67@gmail.com>
2022-12-21 10:26:13 -0500

    Text Ornamentation
    Made the Chest Text more colorful (Feel free to change it if desired)
    Made the Text Bounce up and down (looks like effected by gravity)

--- Assets/TextCon.cs
+++ Assets/TextCon.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class TextCon : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    private float textY, aNum;
     void Start()
     {
-        
+        textY = transform.position.y;
+        aNum = 0.00001f;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float anothaNum = (aNum/Mathf.Abs(aNum)) * (textY + 0.03f - transform.position.y)/250;
+        if(transform.position.y < (textY + 0.03) && transform.position.y > (textY - 0.03)){
+            transform.position = new Vector2(transform.position.x, transform.position.y + aNum + anothaNum);
+        } 
+        else{
+            aNum = -1 * aNum;
+            anothaNum = -1 * anothaNum;
+            transform.position = new Vector2(transform.position.x, transform.position.y + aNum + anothaNum);
+        }
+        
         if(GetComponentInParent<ChestController2>().getIsOpen() == true){
             // print("The Chest is Open");
             Destroy(gameObject);
