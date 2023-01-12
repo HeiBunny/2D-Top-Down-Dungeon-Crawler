d42a8c571b6c514a35938a7c31fda2e432162a7f
jonathanmui <jonathan_mui@student.waylandps.org>
2023-01-12 11:13:54 -0500

    Revert "Dungeon Art"
    This reverts commit 953534ff0911f37b33f7fdd24041e556d5a433f8.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -71,12 +71,6 @@ public class PlayerController : MonoBehaviour
             pc.Activate();
             isActivated = true;
         }
-        if(numWeapon == 2){
-            animator.SetFloat("numWeapon", 2);
-        }
-        if(numWeapon == 1){
-            animator.SetFloat("numWeapon", 1);
-        }
     }
 
         private bool TryMove(Vector2 direction){
@@ -123,14 +117,21 @@ public class PlayerController : MonoBehaviour
     public void SwordAttack(){
         if(numWeapon == 1){
             LockMovement();
-        }
 
-        if(spriteRenderer.flipX == true){
-            swordAttack.AttackLeft();
-        }else{
-            swordAttack.AttackRight();
+            if(spriteRenderer.flipX == true){
+                swordAttack.AttackLeft();
+            }else{
+                swordAttack.AttackRight();
+            }
         }
-        
+        if(numWeapon == 2){
+
+            if(spriteRenderer.flipX == true){
+                swordAttack.AttackLeft();
+            }else{
+                swordAttack.AttackRight();
+            }
+        }
     }
 
     public void EndSwordAttack(){
