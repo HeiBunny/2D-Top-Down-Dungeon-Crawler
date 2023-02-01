ab67e1f41136cecf9a9b289d1789bcfe193a788f
HeiBunny <3105joshua@gmail.com>
2023-02-01 09:42:00 -0500

    Arrow
    Added functioning arrows

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -7,11 +7,27 @@ public class Arrow : MonoBehaviour
 
     public float speed;
     public Vector3 direction;
+    public float damage; 
+    SpriteRenderer sr; 
+
+
+
+    public Arrow(Vector3 direction, float speed){
+        this.speed = speed;
+        this.direction = direction;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        if(direction.y > 0){
+            sr.flipY = true;
+        }
+        if(direction.x < 0){
+            sr.flipX = true;
+        }
+
     }
 
     // Update is called once per frame
@@ -22,5 +38,16 @@ public class Arrow : MonoBehaviour
 
     public void Move(){
         transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Hitbox"){
+             PlayerController player = FindObjectOfType<PlayerController>();
+            if (player != null)
+            {
+                player.Health -= damage;
+            }
+        }
     }
 }
--- Assets/Traps.cs
+++ Assets/Traps.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileMap : MonoBehaviour
+public class Traps : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,12 @@ public class TileMap : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player"){
+            
+        }
     }
 }
